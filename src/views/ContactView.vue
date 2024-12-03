45d6e8364de190c034bca2ea2db11caad87fd5bb
AndoGPP <146361135+AndoGPP@users.noreply.github.com>
2024-12-03 20:09:02 +0100

    update image paths in components to use public directory for consistency

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -8,9 +8,9 @@ import Location from '@/components/LocationComponent.vue'
     <Form
       title="Contact us today, get a savings estimation tomorrow"
       description="Are you ready to start automating your SIEM system? Contact us for an initial talk and we can make a rough estimate on how much resources you may be able to save."
-      backgroundImage="images/siem-automation-deslicer-contact-bg.jpg"
+      backgroundImage=".public/images/siem-automation-deslicer-contact-bg.jpg"
     />
-    <Location image-url="images/stockholm-office.jpg" location="Stockholm" />
+    <Location image-url=".public/images/stockholm-office.jpg" location="Stockholm" />
   </main>
 </template>
 
